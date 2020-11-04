370e49acbdf0cc759648d33f137a93ebb6ec60a0
hussaingt8 <hussain_gt8@hotmail.com>
2020-11-05 10:33:52 +1100

    initial commit

---
+++ whatsapp-mern/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCiAN53uYkuD7cfsU-27l79tVQsBCy_yYg",
+  authDomain: "whatsapp-mern-b74c1.firebaseapp.com",
+  databaseURL: "https://whatsapp-mern-b74c1.firebaseio.com",
+  projectId: "whatsapp-mern-b74c1",
+  storageBucket: "whatsapp-mern-b74c1.appspot.com",
+  messagingSenderId: "443083833137",
+  appId: "1:443083833137:web:66fd7b23624db0878a7a10",
+};
